a6f148c20c8d24247942c6f31ea6c489ac776d2b
Joanna Trojak <joasia405@gmail.com>
2018-08-18 17:15:48 +0200

    prime Numbers python

--- python/primeNumbers.py
+++ python/primeNumbers.py
@@ -5,3 +5,20 @@ Created on Fri Aug 17 16:15:29 2018
 @author: joasi
 """
 
+def isPrime(n): 
+    #Corner case 
+    
+    try:     
+        for i in range(2, n-1):
+            if n % i == 0: 
+                return 'NIE'
+    
+        return 'TAK' 
+    except: 
+            if n <= 1: 
+                return 'NIE' 
+
+print(isPrime(1))
+print(isPrime(2))
+print(isPrime(3))
+print(isPrime(4))
