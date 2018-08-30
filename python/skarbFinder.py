db3cc99dd34d5daa641f32a33243b858b9f2d591
Joanna Trojak <joasia405@gmail.com>
2018-08-30 21:07:14 +0200

    skarbFinder correction

--- python/skarbFinder.py
+++ python/skarbFinder.py
@@ -1,20 +1,22 @@
 def skarbFinder(coordinates, howMany): 
     well = {'x': 0, 'y': 0}
     information = [{'y': +1}, {'y': -1}, {'x': 1}, {'x': -1}]
-    count = 0
+    countY = 0
+    countX = 0
+
     for i in range (0, howMany): 
        direction = information[coordinates[i][0]]
-       if direction['y']: 
-           count += direction['y']*coordinates[i][1]
-    
-       
+       keys = direction.keys()
+       if 'y' in direction: 
+           countY += direction['y']*coordinates[i][1]
+       if 'x' in direction: 
+           countX += direction['x']*coordinates[i][1]
+    treasure = (countX, countY)
+    return treasure
+coordinates = [(0, 1), (0, 2), (1, 2), (2, 3)]
+howMany = len(coordinates)
+print(skarbFinder(coordinates, howMany))
 
-        
-        
-        
-
-   
-print(skarbFinder([(0, 1), (0, 2)], 2))
     
 
 
