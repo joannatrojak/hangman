72ca3c2a0a0ec6b8ccd8c9917e4da8db32d35f90
Joanna Trojak <joasia405@gmail.com>
2018-09-18 09:14:02 +0200

    power c#

---
+++ c#/Power.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Power(double a, double b)
+        {
+            string power = Convert.ToString(Math.Pow(Convert.ToInt32(a), Convert.ToInt32(b)));
+            int length = power.Length; 
+
+            if (length == 1)
+            {
+                Console.WriteLine(power);
+            }
+            if (length > 1)
+            {
+                Console.WriteLine(power[length - 1]); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj podstawę i potęgę: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Power(a, b);
+        }
+    }
+}
