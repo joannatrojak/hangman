e245bcff8bb01b6401c6f982274fc972d13108a9
Joanna Trojak <joasia405@gmail.com>
2018-09-18 11:07:15 +0200

    przedszkolanka c#

---
+++ c#/przedszkolanka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static int nwd(int n, int m)
+        {
+            while (m != 0)
+            {
+                int temp = m;
+                m = n % m;
+                n = temp;
+            }
+            return n;
+        }
+        static int nww(int n, int m)
+        {
+            return (n * m) / nwd(n, m);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nww(12, 15));
+        }
+    }
+}
