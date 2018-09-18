910f360a7c3d48bc232c70535de1014206816bb5
Joanna Trojak <joasia405@gmail.com>
2018-09-18 11:22:03 +0200

    addition c#

---
+++ c#/Addition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ile liczb chcesz dodać: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (count > 0)
+            {
+                Console.WriteLine("Podaj liczby do dodania: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+                count --;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
