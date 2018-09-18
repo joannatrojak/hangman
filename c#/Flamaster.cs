2f1924a940a1252cb43a1b8a839b49c06d860abf
Joanna Trojak <joasia405@gmail.com>
2018-09-18 11:00:18 +0200

    Flamaster c#

---
+++ c#/Flamaster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static string CountLetters(string letters)
+        {
+            HashSet<string> result = new HashSet<string>();
+            foreach (char s in letters)
+            {
+                if (letters.Count(x => x == s) <= 2)
+                {
+                    result.Add(Convert.ToString(s));
+                }
+                
+                if (letters.Count(x => x == s) > 2)
+                {
+                    result.Add(Convert.ToString(letters.Count(x => x == s)));
+                }
+            }
+            return String.Join("", result.ToArray());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountLetters("ABBCCCDDDDEEEEEFGGHIIJKKKL"));
+        }
+    }
+}
