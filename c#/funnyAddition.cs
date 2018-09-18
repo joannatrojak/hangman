567ab9009943f52bd59f1149fc10e3f4d001087a
Joanna Trojak <joasia405@gmail.com>
2018-09-18 12:17:02 +0200

    funnyaddition

---
+++ c#/funnyAddition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static string reverse (string n)
+        {
+            char[] CharArray = n.ToCharArray();
+            Array.Reverse(CharArray);
+            return new String(CharArray);
+        }
+        static string palindrome(int n)
+        {
+            int count = 0;
+            while (n != Convert.ToInt32(reverse(n.ToString())))
+            {
+                n = n + Convert.ToInt32(reverse(n.ToString()));
+                count += 1;
+            }
+            return n.ToString() + " " + count.ToString();
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            string n = Console.ReadLine();
+            Console.WriteLine(reverse(n));
+            Console.WriteLine(palindrome(Convert.ToInt32(n)));
+        }
+    }
+}
