3de2b51ba6bc3d29c03651bab0a56f0c2277a0ef
Joanna Trojak <joasia405@gmail.com>
2018-09-19 10:27:07 +0200

    stringMerge c#

---
+++ c#/StringMerge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static string stringMerge(string a, string b)
+        {
+            List<char> merged = new List<char>();
+            int lengthA = a.Length;
+            int lengthB = b.Length;
+            int i = 0;
+            while (lengthA > 0 || lengthB > 0)
+            {
+                merged.Add(a[i]);
+                lengthA--;
+                merged.Add(b[i]);
+                lengthB--;
+                i++;
+            }
+            return string.Join("", merged.ToArray());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(stringMerge("abs","sfd"));
+        }
+    }
+}
