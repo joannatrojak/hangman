15124be68964dea5019b494503dd85131ad7f0dd
Joanna Trojak <joasia405@gmail.com>
2018-09-19 11:17:12 +0200

    gluttons c#

---
+++ c#/gluttons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static double glutton(int time)
+        {
+            return 86400 / time;
+        }
+        static double howManyBoxes(int box, int[] gluttons)
+        {
+            int sum = 0; 
+            for (int i = 0; i< gluttons.Length; i++)
+            {
+                sum += Convert.ToInt32(glutton(gluttons[i]));
+            }
+            double boxes = sum / box;
+            return boxes;
+        }
+        static void Main(string[] args)
+        {
+            int[] gluttons = { 3600, 1800 };
+            Console.WriteLine(howManyBoxes(10, gluttons));
+        }
+    }
+}
