0f465713c872b1f8f816cb09b5b3c7c0140b83dd
Joanna Trojak <joasia405@gmail.com>
2018-09-19 09:46:17 +0200

    CircularArea c#

---
+++ c#/CircularArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        private static double CircleAr(int d, int r)
+        {
+            const double pi = 3.141592654;
+            return Math.Round(((Math.Pow(d, 2)) - (Math.Pow(r, 2) / 4)) * pi, 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CircleAr(10, 10));
+        }
+    }
+}
