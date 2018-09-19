f7585bb87a1d70dbd48dd07f3a1dc954c13f67e7
Joanna Trojak <joasia405@gmail.com>
2018-09-19 09:33:11 +0200

    array reverse c#

---
+++ c#/Arrayreverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7, 1, 2, 3, 4, 5, 6, 7 };
+            int[] result = array.Distinct().ToArray();
+            Array.Reverse(result);
+            foreach (int value in result)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
