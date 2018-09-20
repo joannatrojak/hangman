915baf1aafdcff056735305e170d20b3d520f28d
Joanna Trojak <joasia405@gmail.com>
2018-09-20 10:33:14 +0200

    dwumian c#

---
+++ c#/dwumian.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static int silnia(int n)
+        {
+            int r = 1; 
+            for (int i = 1; i < n + 1; i++)
+            {
+                r = r * i; 
+            }
+            return r;
+        }
+        static int pascalTriangle(int n, int k)
+        {
+            return silnia(n) / (silnia((n - k)) * silnia(k));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(pascalTriangle(7, 3));
+        }
+    }
+}
