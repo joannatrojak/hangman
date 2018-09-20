f3ed3ffc4b944574915539b4dd6b66829b04e85c
Joanna Trojak <joasia405@gmail.com>
2018-09-20 10:28:42 +0200

    euklides game c#

---
+++ c#/euklidesGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static int euklidesGame(int A, int B)
+        {
+            int count = 0; 
+
+            if (A == B)
+            {
+                count = A + B;
+            }
+
+            if (A > B)
+            {
+                int difference = A - B;
+                count = A - difference;
+            }
+            if (B > A)
+            {
+                int difference = B - A;
+                count = B - difference;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(euklidesGame(2, 4));
+        }
+    }
+}
