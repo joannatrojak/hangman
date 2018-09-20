f294f8bb68b218c7f20610facebce20e3f6b282c
Joanna Trojak <joasia405@gmail.com>
2018-09-20 09:29:59 +0200

    collatz c#

---
+++ c#/Collatz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 0;
+            int steps = 1;
+
+            Console.Write("Enter a number greater than 1: ");
+            string number = Console.ReadLine();
+
+            try
+            {
+                n = Int32.Parse(number);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Try again!");
+                Main();
+            }
+
+            while (n != 1)
+            {
+                if (n%2 == 0)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    n = n * 3 + 1;
+                }
+                steps++;
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("It took {steps} to reach {n} from {number}");
+        }
+    }
+}
