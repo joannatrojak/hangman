55396d9f0fcfb9e9ac126731d88812530ff95ffe
Joanna Trojak <joasia405@gmail.com>
2018-09-20 10:15:51 +0200

    sum less than 200 c#

---
+++ c#/sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj dwie liczby do dodania mniejsze od 200");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            if (a < 200 && b < 200){
+                int suma = a + b;
+                Console.WriteLine(suma);
+            }
+        }
+    }
+}
