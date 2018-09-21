ada9db70abc0bf5b8316a02b07e4350f6052aee6
Joanna Trojak <joasia405@gmail.com>
2018-09-21 08:57:06 +0200

    calculator c#

---
+++ c#/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static int addition(int a, int b)
+        {
+            return a + b;
+        }
+        static int substraction(int a, int b)
+        {
+            return a - b;
+        }
+        static int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        static double division(int a, int b)
+        {
+            try
+            {
+                if (a != 0 || b != 0){
+                    return a / b; 
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An error occured: {0}", e);
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(addition(2, 5));
+            Console.WriteLine(substraction(2, 5));
+            Console.WriteLine(multiplication(2, 5));
+            Console.WriteLine(division(1, 1));
+        }
+    }
+}
