2902e77d5d800d59b452153c73aff47bb98b604f
Joanna Trojak <joasia405@gmail.com>
2018-09-21 09:17:31 +0200

    average velocity c#

---
+++ c#/averageVelocity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static int averageVelocity(int v1, int v2)
+        {
+            return (2 * (v1 * v2)) / (v1 + v2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(averageVelocity(60, 40));
+        }
+    }
+}
