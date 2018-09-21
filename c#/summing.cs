83e0a6c434fd1b3634386094445e70498e29546d
Joanna Trojak <joasia405@gmail.com>
2018-09-21 08:20:37 +0200

    summing with while loop c#

---
+++ c#/summing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int i = 1; 
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                string number = Console.ReadLine(); 
+
+                if (number == "")
+                {
+                    break; 
+                }
+                sum += Convert.ToInt32(number);
+                i += 1;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
