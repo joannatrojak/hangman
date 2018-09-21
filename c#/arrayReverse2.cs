1320b792ad17b8f8539378617422ca3dc081a82c
Joanna Trojak <joasia405@gmail.com>
2018-09-21 08:37:44 +0200

    array reverse c#

---
+++ c#/arrayReverse2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3};
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
