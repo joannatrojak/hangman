9e388eec761e18970510506957a9363472bbd152
Joanna Trojak <joasia405@gmail.com>
2018-09-21 09:06:56 +0200

    half c#

---
+++ c#/half.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void half(int number, string[] strings)
+        {
+            for (int i = 0; i< number; i++)
+            {
+                if (strings[i].Length % 2 == 0)
+                {
+                    string text = strings[i];
+                    Console.WriteLine(text.Substring(0, text.Length/2));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] strings = { "pierwszy", "lubiec", "ktotozrobi" };
+            half(3, strings);
+        }
+    }
+}
