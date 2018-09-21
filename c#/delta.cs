3d658751b8d7eb098f2e1dc212840369f3286ece
Joanna Trojak <joasia405@gmail.com>
2018-09-21 08:31:53 +0200

    delta c#

---
+++ c#/delta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static int delta(double a, double b, double c)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            
+            if (delta > 0)
+            {
+                return 2; 
+            }
+            if (delta == 0)
+            {
+                return 1; 
+            }
+            if (delta < 0)
+            {
+                return 0; 
+            }
+            return -1; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj współczynniki delty: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(delta(a, b, c));
+        }
+    }
+}
