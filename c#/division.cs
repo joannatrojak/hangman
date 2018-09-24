7a19ad7ed675756a9e555eda21db57d9d3e49a79
Joanna Trojak <joasia405@gmail.com>
2018-09-24 08:43:41 +0200

    division c#

---
+++ c#/division.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void division(int n, int x, int y)
+        {
+            List<int> result = new List<int>(); 
+            for (int i = 1; i < n; i++)
+            {
+                if (i % x == 0 && i % y != 0)
+                {
+                    result.Add(i);
+                }
+            }
+            result.ForEach(Console.WriteLine);
+        }
+        static void Main(string[] args)
+        {
+            division(7, 2, 4);
+        }
+    }
+}
