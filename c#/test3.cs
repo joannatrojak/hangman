30fbf1465c5ff5691ad42f0c829c2b9c401a6114
Joanna Trojak <joasia405@gmail.com>
2018-09-24 08:49:10 +0200

    test3 c#

---
+++ c#/test3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(b);
+            int c = 0; 
+            
+            while (true)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a == 42 && b != 42)
+                {
+                    c += 1;
+                }
+                Console.WriteLine(a);
+                b = a;
+                if (c == 3)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
