15b523f015bc55068324bcbe55c2697b3a258d57
Joanna Trojak <joasia405@gmail.com>
2018-09-24 09:17:54 +0200

    average c#

---
+++ c#/average.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void average(int[] list)
+        {
+            int sum = list.Sum();
+            Console.WriteLine(Math.Floor(Convert.ToDouble(sum / list.Length)));
+        }
+        static void Main(string[] args)
+        {
+            int[] list = { 4, 1, 2, 3, 4 };
+            average(list);
+        }
+    }
+}
